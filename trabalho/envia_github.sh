551205ca7731c62697f934cc1f5d78b79d6f7279
BrunaToledoGuedes <brunatoledo30@gmail.com>
2017-06-10 21:00:52 -0400

    msg

--- trabalho/envia_github.sh
+++ trabalho/envia_github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # config nome e email
-git config --global user.name "Bruna Toledo Guedes"
+git config --global user.name "BrunaToledoGuedes"
 git config --global user.email "brunatoledo30@gmail.com"
 
 git add *
